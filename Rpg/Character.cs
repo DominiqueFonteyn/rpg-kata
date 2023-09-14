2e94d3091b50e675ab79de3cd4d0190f55c345f0
slow2serious <slow2serious@gmail.com>
2023-09-14 15:19:56 +0300

    char class +ctor

---
+++ Rpg/Character.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rpg
+{
+    public class Character
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Level { get; set; }
+        public bool Alive { get; set; }
+
+        public Character(string name)
+        {
+            Name = name;
+            Health = 1000;
+            Level = 1;
+            Alive = true;
+        }
+
+    }
+}

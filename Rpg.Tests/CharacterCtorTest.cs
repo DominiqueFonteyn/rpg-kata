0ac20868e130443275cb9ebda1c94f1ef57eba4a
slow2serious <slow2serious@gmail.com>
2023-09-14 15:21:06 +0300

    ctor test

---
+++ Rpg.Tests/CharacterCtorTest.cs
@@ -0,0 +1,15 @@
+﻿namespace Rpg.Tests
+{
+    public class CharacterCtorTest
+    {
+        [Fact]
+        public void CreateCharacter_CorrectValues()
+        {
+            var character = new Character("test");
+            Assert.Equal("test", character.Name);
+            Assert.Equal(1, character.Level);
+            Assert.Equal(1000, character.Health);
+            Assert.True(character.Alive);
+        }
+    }
+}

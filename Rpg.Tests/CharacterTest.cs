95f5b6f55a2bba8c0aed0e4f6a32399c50a2c458
Simon Vereecke <simon.vereecke@protime.eu>
2023-09-14 13:45:00 +0200

    inital test

---
+++ Rpg.Tests/CharacterTest.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace Rpg.Tests
+{
+    public class CharacterTest
+    {
+        [Fact]
+        public void Health_StartsAt1000()
+        {
+            var character = new Character();
+
+            Assert.Equal(1000, character.Health);
+        }
+    }
+
+    public class Character
+    {
+        public Character()
+        {
+        }
+
+        public int Health { get; internal set; }
+    }
+}

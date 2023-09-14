4aa4d612e8bd89d4d95d3b58373f6cc8a28ead2a
Jo Geukens <jo.geukens@protime.eu>
2023-09-14 13:41:19 +0200

    Added health and level to character

---
+++ Rpg.Tests/CharacterTests.cs
@@ -0,0 +1,38 @@
+﻿namespace Rpg.Tests;
+
+public class CharacterTests
+{
+    public CharacterTests()
+    {
+    }
+
+    [Fact]
+    public void WhenCreatingACharacter_ShouldHaveHealthEqual1000()
+    {
+        var character = new Character();
+        
+        Assert.Equal(1000, character.Health);
+    }
+
+    [Fact]
+    public void WhenCreatingACharacter_ShouldBeLevel1()
+    {
+        var character = new Character();
+        
+        Assert.Equal(1, character.Level);
+    }
+}
+
+public class Character
+{
+    private const int StartingHealth = 1000;
+    private const int StartingLevel = 1;
+    public Character()
+    {
+        Health = StartingHealth;
+        Level = StartingLevel;
+    }
+
+    public int Health { get; private set; }
+    public int Level { get; private set; }
+}

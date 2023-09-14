d634525466dafd920996bae3f540a297f4db922f
Jo Geukens <jo.geukens@protime.eu>
2023-09-14 14:24:58 +0200

    Implement damage and healing

--- Rpg.Tests/CharacterTests.cs
+++ Rpg.Tests/CharacterTests.cs
@@ -2,10 +2,6 @@
 
 public class CharacterTests
 {
-    public CharacterTests()
-    {
-    }
-
     [Fact]
     public void WhenCreatingACharacter_ShouldHaveHealthEqual1000()
     {
@@ -29,19 +25,59 @@ public class CharacterTests
         
         Assert.True(character.Alive);
     }
-}
 
-public class Character
-{
-    private const int StartingHealth = 1000;
-    private const int StartingLevel = 1;
-    public Character()
+    [Fact]
+    public void TakeDamage_WhenTakingDamage_CharacterHealthGoesDown()
     {
-        Health = StartingHealth;
-        Level = StartingLevel;
+        var character = new Character();
+
+        character.TakeDamage(100);
+        
+        Assert.Equal(900, character.Health); 
     }
 
-    public int Health { get; private set; }
-    public int Level { get; private set; }
-    public bool Alive => Health > 0;
+    [Fact]
+    public void TakeDamage_WhenTakingMoreDamageThanHealth_CharacterDiesAndHealthIsZero()
+    {
+        var character = new Character();
+        
+        character.TakeDamage(character.Health+1);
+        
+        Assert.Equal(0, character.Health);
+        Assert.False(character.Alive);
+    }
+
+    [Fact]
+    public void Heal_WhenCharacterIsHealed_IncreaseHealth()
+    {
+        var character = new Character();
+        var startingHealth = character.Health;
+        character.TakeDamage(1);
+
+        character.Heal(1);
+        
+        Assert.Equal(character.Health, startingHealth);
+    }
+
+    [Fact]
+    public void Heal_WhenCharacterIsDead_ItCannotBeHealed()
+    {
+        var character = new Character();
+        character.TakeDamage(character.Health);
+        
+        character.Heal(1);
+        
+        Assert.False(character.Alive);
+    }
+
+    [Fact]
+    public void Heal_WhenCharacterIsHealedAbove1000_HealthShouldBe1000()
+    {
+        var character = new Character();
+        character.TakeDamage(1);
+        
+        character.Heal(10);
+        
+        Assert.Equal(1000, character.Health);
+    }
 }
---
+++ Rpg/Character.cs
@@ -0,0 +1,36 @@
+﻿namespace Rpg;
+
+public class Character
+{
+    private const int StartingHealth = 1000;
+    private const int StartingLevel = 1;
+
+    public Character()
+    {
+        Health = StartingHealth;
+        Level = StartingLevel;
+    }
+
+    public int Health { get; private set; }
+    public int Level { get; private set; }
+    public bool Alive => Health > 0;
+
+    public void TakeDamage(int damage)
+    {
+        Health = Health - damage < 0
+            ? 0
+            : Health - damage;
+    }
+
+    public void Heal(int healingAmount)
+    {
+        if (!Alive)
+        {
+            return;
+        }
+
+        Health = Health + healingAmount > StartingHealth 
+            ? StartingHealth
+            : Health + healingAmount; 
+    }
+}

7854e6575c620eeb1aa9f92089cc84ec5999086d
MB-Development <mathieu_broe@yahoo.com>
2023-09-14 14:09:47 +0200

    DoDamage kills of health to low

--- Rpg.Tests/DoDamageShould.cs
+++ Rpg.Tests/DoDamageShould.cs
@@ -14,4 +14,17 @@ public class DoDamageShould
         Assert.Equal(initialHealth-damage, character.Health);
     }
 
+    [Theory]
+    [InlineData(true, 999)]
+    [InlineData(false, 1001)]
+    [InlineData(false, 10001)]
+    public void KillCharacter_WhenHealthBelowOne(bool stillAlive, int damage)
+    {
+        var character = new Character();
+        
+        character.DoDamage(damage);
+        
+        Assert.Equal(stillAlive, character.IsAlive);
+    }
+
 }
--- Rpg/Character.cs
+++ Rpg/Character.cs
@@ -4,10 +4,11 @@ public class Character
 {
     public int Health { get; set; } = 1000;
     public int Level  => 1;
-    public bool IsAlive => true;
+    public bool IsAlive { get; set; } = true;
 
     public void DoDamage(int damage)
     {
         Health -= damage;
+        if (Health <= 0) IsAlive = false;
     }
 }
